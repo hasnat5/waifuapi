647aa8a169c4d98dc74a1af7a066f4a9d9f67a86
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-29 23:39:45 +0700

    add search feature

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Discover from './Discover'
 import jumbotron from '../assets/backdrop.jpg'
 import Trending from './Trending'
+import SearchMovie from './SearchMovie'
 // import DetailMovie from './DetailMovie'
 
 // api key
@@ -14,17 +15,35 @@ import Trending from './Trending'
 // https://api.themoviedb.org/3/movie/550?api_key=0b78155e665ea8730d66deee9862df88
 
 export default class Homepage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            Title: []
+        }
+    }
 
     getData = (data) => {
         console.log(data)
         this.props.onSubmit(data)
     }
 
+    handleSearch(title) {
+        console.log(this.state.Title);
+        this.setState({ Title: title })
+    }
+
+
     render() {
         return (
             <main onSubmit={this.getData} className='container bg-white mx-auto'>
                 <section className='bg-cover h-36 grid mb-8' style={{ backgroundImage: `url(${jumbotron})` }}>
-                    <input type="text" placeholder="Search movie" className="font-poppins justify-self-center mt-28 input input-bordered input-md w-full max-w-xs" />
+                    <input onChange={({ target }) => this.handleSearch(target.value)} type="text" placeholder="Search movie" className="font-poppins justify-self-center mt-28 input input-bordered input-md w-full max-w-xs" />
+                </section>
+
+                <section className=''>
+                    <h1 className='font-poppins px-4 mb-4'>Result Search</h1>
+                    <SearchMovie title={this.state.Title} onSubmit={this.getData} />
                 </section>
 
                 <section className='mb-5'>
@@ -36,6 +55,7 @@ export default class Homepage extends Component {
                     <h1 className='font-poppins px-4 mb-4'>Trending</h1>
                     <Trending onSubmit={this.getData} />
                 </section>
+
 
 
             </main>
---
+++ src/pages/SearchMovie.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import Card from '../components/Card'
+
+export class SearchMovie extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            Search: [],
+        }
+    }
+
+    componentDidMount() {
+        console.log(this.props.title)
+
+        axios.get(`${process.env.REACT_APP_BASE_URL}/search/movie/?query=${this.props.title}`, {
+            params: {
+                api_key: process.env.REACT_APP_TMDB_KEY
+            }
+        })
+            .then(Response => {
+                this.setState({ Search: Response.data.results })
+
+            })
+    }
+
+    componentDidUpdate(pP, pS) {
+        if (pP.title !== this.props.title) {
+            axios.get(`${process.env.REACT_APP_BASE_URL}/search/movie/?query=${this.props.title}`, {
+                params: {
+                    api_key: process.env.REACT_APP_TMDB_KEY
+                }
+            })
+                .then(Response => {
+                    this.setState({ Search: Response.data.results })
+
+                })
+        }
+    }
+
+
+    handleTampil(Film) {
+        console.log(Film.id)
+        this.props.onSubmit(Film.id)
+    }
+
+    render() {
+        return (
+            <section className='container min-w-full' >
+                <div className='grid grid-flow-col-dense overflow-x-scroll'>
+                    {this.state.Search.map((Search, idx) => {
+                        return (
+                            <Card key={idx} data={Search} tampil={this.handleTampil.bind(this, Search)} />
+                        )
+                    })}
+                </div>
+            </ section>
+        )
+    }
+}
+
+export default SearchMovie
